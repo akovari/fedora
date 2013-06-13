189aae813c5486a4f0483f875eb7b0f9b560912c
Axilleas Pipinellis <axilleas@archlinux.gr>
2013-06-13 14:11:23 +0300

    Update deps

--- gitlab-deps/reposearch.sh
+++ gitlab-deps/reposearch.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 fedora_gems_raw='/home/axil/fedora/gitlab-deps/rubygems_fedora_raw'
-fedora_gems_new='/home/axil/fedora/gitlab-deps/rubygems_fedora'
+fedora_gems='/home/axil/fedora/gitlab-deps/rubygems_fedora'
 bugzilla_gems_raw='/home/axil/fedora/gitlab-deps/rubygems_bugzilla_raw'
 bugzilla_gems='/home/axil/fedora/gitlab-deps/rubygems_bugzilla'
 
+# Remove files if already exist
+ls -l | grep rubygem > /dev/null 2>&1
+if [ '$?' = '0' ];
+then 
+  rm rubygems_*
+fi
+
 echo 'Searching Fedora repositories...'
-touch $fedora_gems_raw
+#touch $fedora_gems_raw
 yum search all rubygem | awk '{print $1}' > $fedora_gems_raw
 
 # Striping uneeded symbols and the rubygem- prefix
-sed -e 's/rubygem-//g' -e 's/.noarch//g' -e 's/.x86_64//g' -e '/-doc/d' -e '/i686/d' -e '/==/d' -e '/:/d' < $fedora_gems_raw > $fedora_gems_new
+sed -e 's/rubygem-//g' -e 's/.noarch//g' -e 's/.x86_64//g' -e '/-doc/d' -e '/i686/d' -e '/==/d' -e '/:/d' < $fedora_gems_raw > $fedora_gems
+
+echo 'Done!'
 
 # Install python-bugzilla first
 echo 'Searching Bugzilla for Review Requests...'
 bugzilla query --product=fedora --bug_status=new,assigned --component='Package Review' --short_desc='rubygem-' \
-  | awk 'BEGIN { FS = " - " } ; { print $3 }' | awk 'BEGIN { FS = ":" } ; { print $2 }' | sed -e 's/ rubygem-//' >> $fedora_gems_new
-
-bugzilla query --product=fedora --bug_status=new,assigned --component='Package Review' --short_desc='rubygem-' \
   | awk 'BEGIN { FS = " - " } ; { print $3 }' | awk 'BEGIN { FS = ":" } ; { print $2 }' | sed -e 's/ rubygem-//' > $bugzilla_gems
+
+bugzilla query --product=fedora --bug_status=new,assigned --component='Package Review' --short_desc='rubygem-' > $bugzilla_gems_raw
+
+cat $bugzilla_gems >> $fedora_gems
 
 echo 'Done!'
